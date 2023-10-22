7b3c90e90355b8263935f2f08e2c9667794ac532
Anvarbek <biggamer0055@gmail.com>
2023-10-22 12:29:13 +0500

    qarang header ga rasm qo'ydim kn section-three bn mobile-app ni media ni to'g'irlash kk.Zahro nimadr qo'shamizmi yana ?

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,6 +1,7 @@
 
 let navlist = document.getElementById("navlist");
 navlist.style.maxHeight = "0px";
+navlist.style.padding = "0px";
 
 function togglemenu() {
     if (navlist.style.maxHeight == "0px") {
@@ -14,64 +15,5 @@ function togglemenu() {
 // -----------------------------------------------
 window.addEventListener("scroll", function(){
 var navScroll = document.getElementById("nav");
-navScroll.classList.toggle("fixed", window.scrollY > 20)
+navScroll.classList.toggle("fixed", window.scrollY > 15)
 })
-// ----------------------------------------------
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("cards");
-//   if (n > x.length) {slideIndex = 2}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   x[slideIndex-1].style.display = "block";  
-// }
-
-const aboutfood = document.querySelector('.aboutfood');
-const content_box = document.querySelector('.content_box');
-const iconClose = document.querySelector(".close_about");
-const box = document.querySelector(".box");
-
-// aboutfood.addEventListener('click', () => {
-//   box.style.transform = "rotateY(180deg)";
-// });
-
-// iconClose.addEventListener('click', () => {
-//   box.classList.add('active');
-// });
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
